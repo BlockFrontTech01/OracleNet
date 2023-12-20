82cc929f58b8e083863478f08182ae1624dc906a
Jeff <148919566+jeff96655@users.noreply.github.com>
2023-12-20 10:01:50 +0100

    Update header.js

--- components/header.js
+++ components/header.js
@@ -14,7 +14,7 @@ const Header = () => {
   }, []);
 
   const onDeveloperClick = useCallback(() => {
-    window.open("https://parkingswing.vercel.app");
+    window.open("https://jeffspace.vercel.app");
   }, []);
 
   return (
